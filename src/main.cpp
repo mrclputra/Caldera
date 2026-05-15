7a913adc2098ed337350f87ecf5d5379653c5c77
Marcel Putra <marcelino.ares.putra@gmail.com>
2026-05-16 00:49:25 +0800

    spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	spdlog::set_pattern("[%H:%M:%S - %t] [%^%l%$] %v");
+	spdlog::info("SPDLOG initialized v{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR);
 	return 0;
 }
